e9eedaa572fabf4eadad21b70f860fed5b396bad
Gus Nelson <bananagus@gmail.com>
2015-03-04 22:07:10 -0600

    structure of pydictiterator

--- PyDictIterator.cpp
+++ PyDictIterator.cpp
@@ -1,4 +1,6 @@
 #include "PyDictIterator.h"
+#include <string>
+using namespace std;
 
 PyDictIterator::PyDictIterator() {
 
@@ -8,6 +10,22 @@ PyDictIterator::~PyDictIterator() {
 
 }
 
-PyDictIterator::getType() {
+PyType* PyDictIterator::getType() {
 	return PyTypes[PyDictIteratorType];
+}
+
+string PyDictIterator::toString() {
+	ostringstream s;
+
+	s << "<dict_iterator object at " << this << ">";
+
+	return s.str();
+}
+
+PyObject* PyDictIterator::__iter__(vector<PyObject*>* args) {
+
+}
+
+PyObject* PyListIterator::__next__(vector<PyObject*>* args) {
+
 }
--- PyDictIterator.h
+++ PyDictIterator.h
@@ -1,7 +1,22 @@
+#ifndef PYDICTITERATOR_H
+#define	PYDICTITERATOR_H
+
+#include "PyType.h"
+
 using namespace std;
 
 class PyDictIterator : public PyObject {
 public:
     PyDictIterator();
     virtual ~PyDictIterator();
+
+    PyType* getType();
+    string toString();
+protected:
+    virtual PyObject* __iter__(vector<PyObject*>* args);
+    virtual PyObject* __next__(vector<PyObject*>* args);
+
+
 };
+
+#endif	/* PYCITITERATOR_H */
